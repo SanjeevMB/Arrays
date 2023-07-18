7aab74d1b5f8cec38eb80d2eb4abe5fe0afc1a72
Sanjeev Kumar <sanjeev110592@gmail.com>
2023-07-18 22:33:22 +0530

    Solution added for array transversal 2

--- arrayTransversal.js
+++ arrayTransversal.js
@@ -19,3 +19,44 @@ function arrayTransversal(array, k) {
 let arrayTransversalResult = arrayTransversal([-1, -100, 3, 99], 4);
 
 console.log(arrayTransversalResult);
+
+
+/* ===== Array transversal 2 ===== */
+
+
+function arrayTransversalWithExtraSpace(nums, k) {
+
+    let n = nums.length;
+
+    k = k % n;
+
+    let resultArray = [];
+
+    let startPoint = k;
+
+    let endPoint = n - k
+
+    for (let i = 0; i < n; i++) {
+
+        if (startPoint > 0) {
+
+            resultArray.push(nums[n - startPoint]);
+            startPoint--;
+            endPoint++;
+
+        } else {
+
+            resultArray.push(nums[endPoint - n]);
+            endPoint++;
+
+        }
+
+    }
+
+    return resultArray;
+
+}
+
+let arrayTransversalWithExtraSpaceResult = arrayTransversalWithExtraSpace([1, 2, 3, 4, 5, 6, 7], 10);
+
+console.log(arrayTransversalWithExtraSpaceResult);
